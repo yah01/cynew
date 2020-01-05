2d940f7e8cc16fb9d4dfcc5f7e9dd663cb743c1f
yah01 <kagaminehuan@live.com>
2020-01-05 18:53:29 +0800

    fix a bug about default suffix

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 )
 
 type Config struct {
-	DefaultTemp string
+	DefaultTemp   string
 	DefaultSuffix string
 }
 
 func getSelfPath() string {
-	return os.Args[0][0:len(os.Args[0])-len("cynew.exe")]
+	return os.Args[0][0 : len(os.Args[0])-len("cynew.exe")]
 }
 
 const (
@@ -22,39 +22,39 @@ const (
 )
 
 var (
-	help bool
-	list bool
+	help   bool
+	list   bool
 	create bool
-	temp string
+	temp   string
 	suffix string
-	add string
-	info string
+	add    string
+	info   string
 
 	config Config
 
-	fileDir = getSelfPath()
-	tempDir = fileDir+"/templates"
-	workDir,_ = os.Getwd()
-	configPath = fileDir+"/config.json"
+	fileDir    = getSelfPath()
+	tempDir    = fileDir + "/templates"
+	workDir, _ = os.Getwd()
+	configPath = fileDir + "/config.json"
 )
 
 func init() {
 	configFile, _ := ioutil.ReadFile(configPath)
 	json.Unmarshal(configFile, &config)
 
-	flag.BoolVar(&help,"h",false,"show help information")
-	flag.BoolVar(&list,"ls",false,"list all template(s)")
-	flag.BoolVar(&create,"c",false,"create file(s) without template")
-	flag.StringVar(&temp,"t",config.DefaultTemp,"set default template")
-	flag.StringVar(&suffix,"s",config.DefaultSuffix,"set default suffix")
-	flag.StringVar(&add,"a","","add *filename* into templates")
-	flag.StringVar(&info,"i","","show information of *temp*")
+	flag.BoolVar(&help, "h", false, "show help information")
+	flag.BoolVar(&list, "ls", false, "list all template(s)")
+	flag.BoolVar(&create, "c", false, "create file(s) without template")
+	flag.StringVar(&temp, "t", config.DefaultTemp, "set default template")
+	flag.StringVar(&suffix, "s", config.DefaultSuffix, "set default suffix")
+	flag.StringVar(&add, "a", "", "add *filename* into templates")
+	flag.StringVar(&info, "i", "", "show information of *temp*")
 }
 
 func trimSuffixName(suf string) string {
 	name := []byte(suf)
 
-	for i:=len(name)-1;i>=0;i-- {
+	for i := len(name) - 1; i >= 0; i-- {
 		if name[i] == '.' {
 			name = name[0:i]
 			break
@@ -67,11 +67,11 @@ func trimSuffixName(suf string) string {
 func flagProcess() {
 	flag.Parse()
 
-	if help==true {
+	if help == true {
 		flag.Usage()
 	}
 
-	if list==true {
+	if list == true {
 		dir, _ := ioutil.ReadDir(tempDir)
 
 		for i := 0; i < len(dir); i++ {
@@ -81,43 +81,48 @@ func flagProcess() {
 	}
 
 	config.DefaultTemp = temp
-	config.DefaultSuffix = suffix
+	if config.DefaultSuffix != suffix {
+		config.DefaultSuffix = "."+suffix
+	}
 
 	if add != "" {
-		file,err := ioutil.ReadFile(add)
+		file, err := ioutil.ReadFile(add)
 		if err != nil {
-			fmt.Println("No such file:",add)
+			fmt.Println("No such file:", add)
 		} else {
-			ioutil.WriteFile(tempDir + "/" + trimSuffixName(add), file,Perm)
+			ioutil.WriteFile(tempDir+"/"+trimSuffixName(add), file, Perm)
 		}
 	}
 
 	if info != "" {
 
 	}
+
+	JSON, _ := json.Marshal(&config)
+	ioutil.WriteFile(configPath, JSON, Perm)
 }
 
 func main() {
 	flagProcess()
 
-	if flag.NArg()==1 {
+	if flag.NArg() == 1 {
 		create = true
 	}
 
 	if create == true {
-		for _,name := range flag.Args() {
-			ioutil.WriteFile(workDir+"/"+name,nil,Perm)
+		for _, name := range flag.Args() {
+			ioutil.WriteFile(workDir+"/"+name+config.DefaultSuffix, nil, Perm)
 		}
-	} else {
-		tempName := flag.Arg(flag.NArg()-1)
-		file,err := ioutil.ReadFile(tempDir+"/"+tempName)
+	} else if flag.NArg() > 0 {
+		tempName := flag.Arg(flag.NArg() - 1)
+		file, err := ioutil.ReadFile(tempDir + "/" + tempName)
 
 		if err != nil {
-			fmt.Println("No such templates:",tempName)
+			fmt.Println("No such templates:", tempName)
 		} else {
-			for i:=0;i<flag.NArg()-1;i++ {
+			for i := 0; i < flag.NArg()-1; i++ {
 				name := flag.Arg(i)
-				ioutil.WriteFile(workDir+"/"+name, file, Perm)
+				ioutil.WriteFile(workDir+"/"+name+config.DefaultSuffix, file, Perm)
 			}
 		}
 	}
