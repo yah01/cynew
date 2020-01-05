2cf9277888b682101400561fd7402c9a6e67babd
yah01 <kagaminehuan@live.com>
2020-01-05 18:35:43 +0800

    parse flag args by package flag and add functions

--- README.md
+++ README.md
@@ -5,16 +5,30 @@
 I highly recommend that you add the directory of cynew into environment variable **Path** such that you could run it anywhere.
 
 ### simply create a new file named *hello.cpp*
-> cynew hello.cpp
+> cynew *hello.cpp*
 
 ### create a new file *hello.cpp* with a template *temp*
-> cynew hello.cpp temp
+> cynew *hello.cpp* *temp*
+
+### create more files *file1,file2,...* with the same template *temp*
+> cynew *file1* *file2* ... *temp*
+
+### create more files *file1,file2,...* without any template
+> cynew *file1* *file2* ... -c
 
 ### set the default template to *temp*
-> cynew -t temp
+> cynew -t *temp*
 
 ### set the default suffix to *.cpp*
-> cynew -s cpp
+> cynew -s *cpp*
 
 ### list all template(s)
 > cynew -ls
+
+## flag
+- [-ls]: list all template(s)
+- [-c]: create file(s) without template
+- [-t *temp*]: set default template to *temp*
+- [-s *suf*]: set default suffix to *.suf*
+- [-a *filename*]: add *filename* into templates
+- [-i *temp*]: show information of *temp*
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -9,53 +10,114 @@ import (
 
 type Config struct {
 	DefaultTemp string
+	DefaultSuffix string
 }
 
 func getSelfPath() string {
 	return os.Args[0][0:len(os.Args[0])-len("cynew.exe")]
 }
 
-func main() {
-	var config Config
-	configFile, _ := ioutil.ReadFile(getSelfPath() + "/config.json")
+const (
+	Perm = 0644
+)
+
+var (
+	help bool
+	list bool
+	create bool
+	temp string
+	suffix string
+	add string
+	info string
+
+	config Config
+
+	fileDir = getSelfPath()
+	tempDir = fileDir+"/templates"
+	workDir,_ = os.Getwd()
+	configPath = fileDir+"/config.json"
+)
+
+func init() {
+	configFile, _ := ioutil.ReadFile(configPath)
 	json.Unmarshal(configFile, &config)
-	path, _ := os.Getwd()
 
-	switch len(os.Args) {
-	case 2:
-		switch os.Args[1] {
-		case "-ls":
-			dir, _ := ioutil.ReadDir(getSelfPath())
+	flag.BoolVar(&help,"h",false,"show help information")
+	flag.BoolVar(&list,"ls",false,"list all template(s)")
+	flag.BoolVar(&create,"c",false,"create file(s) without template")
+	flag.StringVar(&temp,"t",config.DefaultTemp,"set default template")
+	flag.StringVar(&suffix,"s",config.DefaultSuffix,"set default suffix")
+	flag.StringVar(&add,"a","","add *filename* into templates")
+	flag.StringVar(&info,"i","","show information of *temp*")
+}
 
-			for i := 0; i < len(dir); i++ {
-				switch dir[i].Name() {
-				case "config.json", "cynew.exe":
-					continue
+func trimSuffixName(suf string) string {
+	name := []byte(suf)
 
-				default:
-					fmt.Println(dir[i].Name())
-				}
-			}
-
-		default:
-			fmt.Println("create file", os.Args[1])
-			ioutil.WriteFile(path+"/"+os.Args[1], nil, 0644)
+	for i:=len(name)-1;i>=0;i-- {
+		if name[i] == '.' {
+			name = name[0:i]
+			break
 		}
-	case 3:
-		switch os.Args[1] {
-		case "-d":
-			config.DefaultTemp = os.Args[2]
-			configFile, _ = json.Marshal(config)
-			ioutil.WriteFile(getSelfPath()+"/config.json", configFile, 0644)
-		default:
-			template, err := ioutil.ReadFile(getSelfPath() + "/" + os.Args[2])
+	}
 
-			if err != nil {
-				fmt.Println("not find template", os.Args[2])
-				ioutil.WriteFile(path+"/"+os.Args[1], nil, 0644)
-			} else {
-				fmt.Println("create template", os.Args[2])
-				ioutil.WriteFile(path+"/"+os.Args[1], template, 0644)
+	return string(name)
+}
+
+func flagProcess() {
+	flag.Parse()
+
+	if help==true {
+		flag.Usage()
+	}
+
+	if list==true {
+		dir, _ := ioutil.ReadDir(tempDir)
+
+		for i := 0; i < len(dir); i++ {
+
+			fmt.Println(dir[i].Name())
+		}
+	}
+
+	config.DefaultTemp = temp
+	config.DefaultSuffix = suffix
+
+	if add != "" {
+		file,err := ioutil.ReadFile(add)
+		if err != nil {
+			fmt.Println("No such file:",add)
+		} else {
+			ioutil.WriteFile(tempDir + "/" + trimSuffixName(add), file,Perm)
+		}
+	}
+
+	if info != "" {
+
+	}
+}
+
+func main() {
+	flagProcess()
+
+	if flag.NArg()==1 {
+		create = true
+	}
+
+	if create == true {
+		for _,name := range flag.Args() {
+			ioutil.WriteFile(workDir+"/"+name,nil,Perm)
+		}
+	} else {
+		tempName := flag.Arg(flag.NArg()-1)
+		file,err := ioutil.ReadFile(tempDir+"/"+tempName)
+
+		if err != nil {
+			fmt.Println("No such templates:",tempName)
+		} else {
+			for i:=0;i<flag.NArg()-1;i++ {
+				name := flag.Arg(i)
+				ioutil.WriteFile(workDir+"/"+name, file, Perm)
 			}
 		}
 	}
