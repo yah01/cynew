f1dbd25c9035f52dfe6c757dc666dd6a15945dc7
cenyang <cenyang@bytedance.com>
2020-04-09 02:18:29 +0800

    fix wrong path constant

--- type/const.go
+++ type/const.go
@@ -9,14 +9,15 @@ const (
 	Perm      = 0644
 	Separator = string(os.PathSeparator)
 
-	TemplateType_Empty      TemplateType = 0	// for creating a empty file
-	TemplateType_SingleFile TemplateType = 1	// for creating single file with template
-	TemplateType_Project    TemplateType = 2	// for creating a folder with template
+	TemplateType_Empty      TemplateType = 0 // for creating a empty file
+	TemplateType_SingleFile TemplateType = 1 // for creating single file with template
+	TemplateType_Project    TemplateType = 2 // for creating a folder with template
 )
 
 var (
-	FileDir     = os.Args[0][:len(os.Args[0])-len("cynew")]
-	TemplateDir = FileDir + "/templates"
+	tempPath, _ = os.Executable()
+	FileDir     = tempPath[:len(tempPath)-len("/cynew")]
+	TemplateDir = FileDir + Separator + "templates"
 	WorkDir, _  = os.Getwd()
 
 	WaitAllGoroutine = sync.WaitGroup{}
