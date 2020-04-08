6ff86c6e6709fe42706b31c85ea0e7a5e25d76b4
cenyang <cenyang@bytedance.com>
2020-04-09 02:18:47 +0800

    add CreateFileTemplate method

--- store/store.go
+++ store/store.go
@@ -42,12 +42,55 @@ func CreateDir(path string, content TemplateContent) {
 	}
 }
 
-func CreateFile(path string,content TemplateContent) {
+func CreateFile(path string, content TemplateContent) {
 	defer WaitAllGoroutine.Done()
-	newFile,_ := os.Create(path)
+	newFile, _ := os.Create(path)
 	if !content.IsEmpty() {
 		file := content.GetFileList()[0]
 		newFile.Write(file.Content)
 	}
 	newFile.Close()
+}
+
+func CreateFileTemplate(fileName string, content []byte) {
+	var (
+		templateName string
+		info         string
+		template     Template
+	)
+
+	fmt.Print("Template name: ")
+	fmt.Scanln(&templateName)
+	if templateName == "" {
+		templateName = fileName
+	}
+	fmt.Print("Template information (no necessary): ")
+	fmt.Scanln(&info)
+
+	jsonFile, err := os.Create(TemplateDir + Separator + templateName + ".json")
+	if err != nil {
+		fmt.Println("Create template", templateName, "error:", err)
+		return
+	}
+
+	template = Template{
+		Name:    templateName,
+		Info:    info,
+		Type:    TemplateType_SingleFile,
+		Folders: nil,
+		Files: []File{
+			{
+				Name:    "",	// Name is unused for template with type TemplateType_SingleFile
+				Content: content,
+			},
+		},
+	}
+
+	templateJson,err := json.Marshal(template)
+	jsonFile.Write(templateJson)
+	jsonFile.Close()
+}
+
+func CreateFolderTemplate(folderName string, content []os.FileInfo) {
+
 }
