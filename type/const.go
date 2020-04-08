2e3018f3348bf317bd16ceb4662ec61d079acc40
cenyang <cenyang@bytedance.com>
2020-04-09 00:51:25 +0800

    move dir constant to const.go, add TemplateType enum

--- type/const.go
+++ type/const.go
@@ -1,11 +1,23 @@
 package cynew
 
-import "os"
+import (
+	"os"
+	"sync"
+)
 
 const (
-	Folder_Root rootType = "Folder"
-	File_Root   rootType = "File"
-
-	Perm = 0644
+	Perm      = 0644
 	Separator = string(os.PathSeparator)
+
+	TemplateType_Empty      TemplateType = 0
+	TemplateType_SingleFile TemplateType = 1
+	TemplateType_Project    TemplateType = 2
+)
+
+var (
+	FileDir     = os.Args[0][:len(os.Args[0])-len("cynew.exe")]
+	TemplateDir = FileDir + "/templates"
+	WorkDir, _  = os.Getwd()
+
+	WaitAllGoroutine = sync.WaitGroup{}
 )
